5ce1f8ba3e5a8d85cd0183c99db2009a8d76b2cf
acg78219 <562713187@qq.com>
2026-01-12 13:47:59 +0800

    hierarchy删除、拷贝节点的undo和redo

--- src/3d/core/utils/nodeCRUD.ts
+++ src/3d/core/utils/nodeCRUD.ts
@@ -6,6 +6,23 @@ import { useScene } from '@/store/useScene';
 import { ArrayUtils } from '@/utils/Array';
 import { Node, TransformNode } from '@babylonjs/core';
 
+class deletedSerializedNodeClass{
+  constructor(_rawNode:Node, _serializedNode: CC.ObjectNode, _parent: Node | null) {
+    this.rawNode = _rawNode;
+    this.serializedNode = _serializedNode;
+    this.parent = _parent;
+    // setEnabled保证clone体不显示在Scene中,remove保证保存时不序列化进去
+    this.rawNode.setEnabled(false);
+    this.rawNode._removeFromSceneRootNodes();
+  }
+
+  public rawNode: Node | null = null;
+  public serializedNode : CC.ObjectNode | null = null;
+  public parent: Node | null = null;
+}
+
+let deletedSerializedNode : deletedSerializedNodeClass | null;
+
 /**
  * BJS和Hierarchy中Node的增删改查
  */
@@ -62,9 +79,9 @@ export function nodeCRUD() {
       }
     }
 
-    if (!clonedNode.name.toLocaleLowerCase().endsWith('(clone)')) {
-      clonedNode.name = clonedNode.name + '(clone)';
-    }
+    //if (!clonedNode.name.toLocaleLowerCase().endsWith('(clone)')) {
+    //  clonedNode.name = clonedNode.name + '(clone)';
+    //}
     // hierarchy层面添加
     useScene().addHierarchy(clonedNode, parentNode || null);
     return clonedNode;
@@ -73,24 +90,41 @@ export function nodeCRUD() {
   /**
    * 在BJS和Hierarchy中删除该Node
    */
-  function deleteNode(node: Node) {
-    // 1.在层级面板删除
+  async function deleteNode(node: Node) {
+    // 1. 先把删除的Node序列化保存，方便redo
+    deletedSerializedNode = new deletedSerializedNodeClass(node.clone(node.name, node.parent), await copyNode(node), node.parent);
+    // 2.在层级面板删除
     useScene().removeHierarchy(node);
-    // 2.在bjs中删除该Node
+    // 3.在bjs中删除该Node
     Editor.Instance.Scene.getNodes()
       .find((x) => x.uuid == node.uuid)
       ?.dispose();
   }
 
   /**
+   * 将删除的节点重新弄回来
+   * 现在做法是删除的时候clone源数据然后序列化，redo时候反序列化
+   * warning:有可能自定义数据没有被clone或者序列化进去
+   */
+  async function restoreNode():Promise<Node> {
+    // 反序列化之前保存的删除节点的数据
+    if(deletedSerializedNode) {
+      // todo:顺序
+      const node = await pasteNode(deletedSerializedNode.serializedNode, deletedSerializedNode.parent);
+      deletedSerializedNode.rawNode.dispose();
+      deletedSerializedNode = null;
+      return node;
+    }
+  }
+  /**
    * 更新 Node 的新层级
    */
   function updateNodeHierarchy(
     node: Node,
-    newParent: Node | null,
+    targetPosNode: Node | null,
     type: 'before' | 'after' | 'inner',
   ) {
-    const nodeNewParent = ['before', 'after'].includes(type) ? newParent.parent : newParent;
+    const nodeNewParent = ['before', 'after'].includes(type) ? targetPosNode.parent : targetPosNode;
     if(node instanceof TransformNode){
       node.setParent(nodeNewParent);
     }
@@ -101,7 +135,7 @@ export function nodeCRUD() {
     // 保证下次进来的顺序和 ElTree 一样
     // @ts-ignore
     const children = nodeNewParent ? nodeNewParent._children : Editor.Instance.Scene.rootNodes;
-    switchNodePosInParent(node, newParent, type, children);
+    switchNodePosInParent(node, targetPosNode, type, children);
   }
 
   function switchNodePosInParent(from: Node, to: Node, type: 'before' | 'after' | 'inner', children: Node[]) {
@@ -135,6 +169,7 @@ export function nodeCRUD() {
     copyNode,
     pasteNode,
     deleteNode,
+    restoreNode,
     updateNodeHierarchy,
   };
 }
